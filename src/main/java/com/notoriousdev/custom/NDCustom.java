773bd7d709c82d1d3c1c67e6cc7bf8207170a159
BranicYeti <branicyeti@live.co.uk>
2013-03-06 17:00:39 +0000

    Initial Commit

---
+++ src/main/java/com/notoriousdev/custom/NDCustom.java
@@ -0,0 +1,17 @@
+package com.notoriousdev.custom;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class NDCustom extends JavaPlugin
+{
+
+    @Override
+    public void onDisable()
+    {
+    }
+
+    @Override
+    public void onEnable()
+    {
+    }
+}
