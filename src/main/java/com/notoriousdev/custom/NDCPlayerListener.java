bc9ada7a5a06cec4956fbd7d36b878459444c59e
GunfighterJ <joseph.jenniges@gmail.com>
2013-03-09 08:58:52 -0600

    Cleanup

--- src/main/java/com/notoriousdev/custom/NDCPlayerListener.java
+++ src/main/java/com/notoriousdev/custom/NDCPlayerListener.java
@@ -20,42 +20,50 @@ import org.bukkit.event.player.PlayerKickEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.inventory.ItemStack;
 
-public class NDCPlayerListener implements Listener {
+public class NDCPlayerListener implements Listener
+{
 
     public NDCustom plugin;
 
-    public NDCPlayerListener(NDCustom plugin) {
+    public NDCPlayerListener(NDCustom plugin)
+    {
         this.plugin = plugin;
     }
 
     @EventHandler
-    public void onPlayerJoin(final PlayerJoinEvent event){
+    public void onPlayerJoin(final PlayerJoinEvent event)
+    {
         event.setJoinMessage(ChatColor.GREEN + "Join: " + ChatColor.GOLD + event.getPlayer().getName());
 
     }
+
     @EventHandler
-    public void onPlayerQuit(final PlayerQuitEvent event){
+    public void onPlayerQuit(final PlayerQuitEvent event)
+    {
         event.setQuitMessage(ChatColor.RED + "Quit: " + ChatColor.GOLD + event.getPlayer().getName());
 
     }
+
     @EventHandler
-    public void onPlayerKick(final PlayerKickEvent event){
+    public void onPlayerKick(final PlayerKickEvent event)
+    {
         event.setLeaveMessage(ChatColor.RED + "Kick: " + ChatColor.GOLD + event.getPlayer().getName());
 
     }
+
     @EventHandler(priority = EventPriority.HIGHEST)
     public void onPotionThrow(PlayerInteractEvent event)
     {
         Player player = event.getPlayer();
         ItemStack item = player.getItemInHand();
-        if((item.getType() == Material.POTION) || item.getType() == Material.EXP_BOTTLE  && (event.getAction() == Action.RIGHT_CLICK_AIR || event.getAction() == Action.RIGHT_CLICK_BLOCK) && player.getGameMode() == GameMode.CREATIVE)
+        if ((item.getType() == Material.POTION) || item.getType() == Material.EXP_BOTTLE && (event.getAction() == Action.RIGHT_CLICK_AIR || event.getAction() == Action.RIGHT_CLICK_BLOCK) && player.getGameMode() == GameMode.CREATIVE)
         {
             player.sendMessage(ChatColor.RED + "You cannot use potions in creative!");
             event.setCancelled(true);
             player.getInventory().setItemInHand(null);
             player.updateInventory();
         }
-        if((item.getType() == Material.MONSTER_EGG || item.getType() == Material.MONSTER_EGGS) && (event.getAction() == Action.RIGHT_CLICK_BLOCK))
+        if ((item.getType() == Material.MONSTER_EGG || item.getType() == Material.MONSTER_EGGS) && (event.getAction() == Action.RIGHT_CLICK_BLOCK))
         {
             event.setCancelled(true);
             player.sendMessage(ChatColor.RED + "You cannot use monster eggs in creative!");
@@ -65,7 +73,7 @@ public class NDCPlayerListener implements Listener {
     @EventHandler(priority = EventPriority.HIGHEST)
     public void onBowFire(EntityShootBowEvent event)
     {
-        if(event.getEntity() instanceof Player)
+        if (event.getEntity() instanceof Player)
         {
             Player player = (Player) event.getEntity();
             if (player.getGameMode() == GameMode.CREATIVE)
@@ -79,23 +87,24 @@ public class NDCPlayerListener implements Listener {
     @EventHandler(priority = EventPriority.HIGHEST)
     public void onDispenserInteract(InventoryClickEvent event)
     {
-        Player player = (Player)event.getWhoClicked();
+        Player player = (Player) event.getWhoClicked();
         ItemStack item = event.getCurrentItem();
-        if (item != null) {
-            if (player.getGameMode() == GameMode.CREATIVE && event.getInventory().getType().equals(InventoryType.DISPENSER) && item.getType() == Material.POTION || item.getType() == Material.EXP_BOTTLE || item.getType() == Material.MONSTER_EGG || item.getType() == Material.MONSTER_EGGS && (event.isLeftClick() || event.isRightClick() || event.isShiftClick()))
-            {
-                event.setCancelled(true);
-                event.setCursor(null);
-                event.setCurrentItem(null);
-                player.sendMessage(ChatColor.RED + "You cannot put " + item.getType().name().toLowerCase().replace("_"," ") + "s into dispensers!");
-            }
+        if (item != null && player.getGameMode() == GameMode.CREATIVE && event.getInventory().getType().equals(InventoryType.DISPENSER)
+                && (item.getType() == Material.POTION || item.getType() == Material.EXP_BOTTLE || item.getType() == Material.MONSTER_EGG || item.getType() == Material.MONSTER_EGGS)
+                && (event.isLeftClick() || event.isRightClick() || event.isShiftClick()))
+        {
+            event.setCancelled(true);
+            event.setCursor(null);
+            event.setCurrentItem(null);
+            player.sendMessage(ChatColor.RED + "You cannot put " + item.getType().name().toLowerCase().replace("_", " ") + "s into dispensers!");
         }
+
     }
 
     @EventHandler(priority = EventPriority.HIGHEST)
     public void onCreativeDamage(EntityDamageByEntityEvent event)
     {
-        if(event.getDamager() instanceof Player)
+        if (event.getDamager() instanceof Player)
         {
             event.setCancelled(true);
             ((Player) event.getDamager()).sendMessage(ChatColor.RED + "You cannot PVP others while in creative!");
@@ -106,13 +115,12 @@ public class NDCPlayerListener implements Listener {
     public void onSkyblockDeath(PlayerDeathEvent event)
     {
         Player player = event.getEntity();
-        if(player.getLastDamageCause().getCause() == EntityDamageEvent.DamageCause.VOID)
+        if (player.getLocation().getWorld().getName().equalsIgnoreCase("skyblock"))
         {
-            if(player.getLocation().getWorld().getName().equalsIgnoreCase("skyblock"))
+            if (player.getLastDamageCause().getCause() == EntityDamageEvent.DamageCause.VOID)
             {
                 event.setDeathMessage(ChatColor.RED + player.getDisplayName() + " couldn't handle the skyblock.");
-            }
-            else
+            } else
             {
                 event.setDeathMessage(ChatColor.RED + player.getDisplayName() + " died of unknown causes...");
             }
